237ce811c3831646c80d7dfae6eeeb0fcc0b8518
Jamie Friesen Byer <byerjamie@gmail.com>
2021-08-05 14:16:11 -0700

    start of part 4: selectable hazards

--- MindMap/app.js
+++ MindMap/app.js
@@ -1,5 +1,6 @@
 'use strict'
 
+//Drag and Drop
 
 function allowDrop(ev) {
     ev.preventDefault();
@@ -14,6 +15,38 @@ function allowDrop(ev) {
     var data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data));
   }
+
+//For debugging
+//document.getElementById('log').innerHTML += hazard_type;
+
+
+//global var cause_hazard = '';
+
+//Page 4 click
+function select_cause_hazard(hazard_type) {
+  
+  var cause_hazards = ['cause_earthquakes', 'cause_storms', 'cause_landslides', 'cause_space', 'cause_volcanoes', 'cause_waves'];
+  for (let i = 0; i < cause_hazards.length; i++) {
+    if (hazard_type == cause_hazards[i]){
+      document.getElementById(cause_hazards[i]).style.border = '3px solid skyblue';
+    }
+    else {
+      document.getElementById(cause_hazards[i]).style.border = '';
+    }
+  }
+}
+
+function select_effect_hazard(hazard_type) {
+  var effect_hazards = ['effect_earthquakes', 'effect_storms', 'effect_landslides', 'effect_space', 'effect_volcanoes', 'effect_waves'];
+  for (let i = 0; i < effect_hazards.length; i++) {
+    if (hazard_type == effect_hazards[i]){
+      document.getElementById(effect_hazards[i]).style.border = '3px solid skyblue';
+    }
+    else {
+      document.getElementById(effect_hazards[i]).style.border = '';
+    }
+  }
+}
 
 
 //Pagination
@@ -74,7 +107,6 @@ function gotoPage(pageNum) {
 
 //Update the page so it only shows the relevant elements
 function update_page(current_page) {
-  //document.getElementById('log').innerHTML += current_page;
   // Set elements hidden first so that sections that belong to multiple pages are displayed
   if (current_page == 1) {
     var other_pages = document.querySelectorAll('.page2, .page3, .page4, .page5, .page6');
@@ -110,11 +142,9 @@ function update_page(current_page) {
 
 function update_visibility(other_pages, current_page) {
     for (let i = 0; i < other_pages.length; i++) {
-      //other_pages[i].style.visibility = 'hidden';
       other_pages[i].style.display = 'none';
     }
     for (let i = 0; i < current_page.length; i++) {
-      //current_page[i].style.visibility = 'visible';
       current_page[i].style.display = 'block';
     }
 }
